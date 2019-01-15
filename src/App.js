2284d7df0f56a681ef3d4ac13501d64a848b4d2d
Israel Gutiérrez Rojas <gootyfer@gmail.com>
2019-01-15 16:23:53 +0100

    Add lots of errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
-import PokemonList from './PokemonList';
-import './App.css';
+import React, { Component } from "react";
+import * as pokemonData from "./data";
+import PokemonList from "./PokemonList";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    this.state = { pokemonData };
+  }
+
   render() {
     return (
       <main>
         <h1 className="main-title">Mi lista de pokemon</h1>
-        <PokemonList pokemon={this.props.pokemon} />
+
+        <PokemonList pokemon={this.state.pokemon.pokemonData} />
       </main>
     );
   }
